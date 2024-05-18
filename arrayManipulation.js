55c3997e7efdd08aa7549c2a2acaf2e4ca67e0b6
Mickeyb12 <michelbbiney12@gmail.com>
2024-05-18 12:49:43 +0000

    Task 1

---
+++ arrayManipulation.js
@@ -0,0 +1,19 @@
+// arrayManipulation.js
+
+/**
+ * Function that processes an array of numbers.
+ * Each even number is squared and each odd number is tripled.
+ */
+function processArray(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            result.push(arr[i] * arr[i]);
+        } else {
+            result.push(arr[i] * 3);
+        }
+    }
+    return result;
+}
+
+
